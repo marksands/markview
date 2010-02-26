b6f44dbf55c5f003f64e78fb85010c2e0e21978f
marksands <marksands07@gmail.com>
2010-02-26 17:57:48 -0600

    Added tests.

---
+++ test/render_test.rb
@@ -0,0 +1,21 @@
+require 'contest'
+require 'vegas'
+require 'markview'
+
+class RenderTest < Test::Unit::TestCase
+  
+  test "renders given file" do
+    ARGV[0] = 'README.md'
+    assert_equal RDiscount.new(File.read(ARGV[0])).to_html, Markview::Application.markdown_me
+  end
+  
+  test "renders discovered file" do
+    ARGV[0] = Dir.glob("*.{md,markdown}")[0]
+    assert_equal RDiscount.new(File.read(ARGV[0])).to_html, Markview::Application.markdown_me    
+  end
+  
+  test "Vegas runs application" do
+    Vegas::Runner.new(Markview::Application, 'markview', :foreground => true, :skip_launch => true, :debug => true)
+  end
+
+end
