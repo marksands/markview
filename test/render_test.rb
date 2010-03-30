61e0be55c2bf24232eab27de40fdf445b31068e7
Mark Sands <marksands07@gmail.com>
2010-03-29 19:32:29 -0500

    whitespace

--- test/render_test.rb
+++ test/render_test.rb
@@ -3,12 +3,14 @@ require 'test/test_helper'
 class RenderTest
   
   test "renders given file" do
-    assert_equal GitHub::Markup.render(@argv, File.read(@argv)), Markview::Application.markdown_me
+    assert_equal GitHub::Markup.render(@argv, File.read(@argv)),
+      Markview::Application.markdown_me
   end
 
   test "renders discovered file" do
     discover_file
-    assert_equal GitHub::Markup.render(@argv, File.read(@argv)), Markview::Application.markdown_me    
+    assert_equal GitHub::Markup.render(@argv, File.read(@argv)),
+      Markview::Application.markdown_me    
   end
 
   test "no markdown file given or found raises error" do
