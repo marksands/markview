f4b930056db572cce33b043cc7b80b5aabc66b0a
Mark Sands <marksands07@gmail.com>
2011-01-06 20:59:47 -0600

    if file doesn't exist, create and edit in browser

--- lib/markview.rb
+++ lib/markview.rb
@@ -13,7 +13,7 @@ class Markview < Sinatra::Base
   configure do
     @@markup = ARGV[0] ||= Dir.glob("README*")[0]
     unless File.file?(@@markup)
-      raise LoadError, "Failed to open document. Please specify a file."; exit!
+      File.new(@@markup, "w+")
     end
   end
 
